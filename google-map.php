8a25fcb95b0e7f9e9b289dbebf55755d0b6e9b13
Michael <contact@eewee.fr>
2020-03-14 17:16:11 +0100

    Google maps.

--- google-map.php
+++ google-map.php
@@ -3,20 +3,33 @@
 <div class="row">
   <div class="col-md-8">
     <h1>
-      Utilisation de Google Font
+      Utilisation de Google Map
     </h1>
     <p>
-      Nullam vel sem. Duis leo. Pellentesque ut neque. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo.Curabitur turpis. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Ut leo. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. In ac felis quis tortor malesuada pretium.
+      <iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=excelia%20la%20rochelle&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </p>
+    
+    <p>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2764.5055409215156!2d-1.1582859481054288!3d46.14067847901216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480153c41a81db03%3A0xf1d6cb8fcf9a73b3!2sExcelia%20Group!5e0!3m2!1sfr!2sfr!4v1584200641578!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </p>
+    
+    <p>
+      Google map impose à présent l'utilisation d'un compte payant pour réaliser des requêtes vers son API :<br>
+      <iframe width="600" height="450" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/directions?origin=place_id:ChIJA9uBGsRTAUgRs3Oaz4_L1vE&destination=place_id:ChIJiUN9X_XaVA0RJqyVF7JSDGE&key=..." allowfullscreen></iframe>       </p>
+    
+    <p>
+      Utiliser une solution alternative : Open Street Map.
     </p>
   </div>
   <div class="col-md-4">
 
     <div class="card" style="width: 18rem;">
-      <img src="/images/google-font.png" class="card-img-top" alt="Logo Digital School Excelia Group">
+      <img src="/images/google-map.jpg" class="card-img-top" alt="Logo Google Map">
       <div class="card-body">
-        <h5 class="card-title">Google Font</h5>
-        <p class="card-text">Utilisation de “Google Font” dans un projet de site internet. Après avoir récupérer une “Font” sur le site de “Google”, nous allons l’utiliser sur cette page.</p>
-        <a href="https://fonts.google.com" target="_blank" class="btn btn-primary">En savoir +</a>
+        <h5 class="card-title">Google Map</h5>
+        <p class="card-text">Utilisation de “Google Map” dans un projet de site internet. Nous allons afficher une carte avec l'adresse d'Excelia Group La Rochelle sur cette page.</p>
+        <a href="https://developers.google.com/maps/documentation/embed/guide?hl=fr" target="_blank" class="btn btn-primary">En savoir +</a>
       </div>
     </div>
 
