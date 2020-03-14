f418b03fa5abe3069a72a637b533ad7566d80441
Michael <contact@eewee.fr>
2020-03-14 17:37:03 +0100

    Youtube.

--- youtube.php
+++ youtube.php
@@ -3,20 +3,20 @@
 <div class="row">
   <div class="col-md-8">
     <h1>
-      Utilisation de Google Font
+      Utilisation de Youtube
     </h1>
     <p>
-      Nullam vel sem. Duis leo. Pellentesque ut neque. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo.Curabitur turpis. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Ut leo. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. In ac felis quis tortor malesuada pretium.
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/oxLz4HIRqzU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </p>
   </div>
   <div class="col-md-4">
 
     <div class="card" style="width: 18rem;">
-      <img src="/images/google-font.png" class="card-img-top" alt="Logo Digital School Excelia Group">
+      <img src="/images/youtube.jpg" class="card-img-top" alt="Logo Youtube">
       <div class="card-body">
-        <h5 class="card-title">Google Font</h5>
-        <p class="card-text">Utilisation de “Google Font” dans un projet de site internet. Après avoir récupérer une “Font” sur le site de “Google”, nous allons l’utiliser sur cette page.</p>
-        <a href="https://fonts.google.com" target="_blank" class="btn btn-primary">En savoir +</a>
+        <h5 class="card-title">Youtube</h5>
+        <p class="card-text">Utilisation de “Youtube” dans un projet de site internet. Intégrer une vidéo Youtube sur la page.</p>
+        <a href="https://support.google.com/youtube/answer/171780?hl=fr" target="_blank" class="btn btn-primary">En savoir +</a>
       </div>
     </div>
 
